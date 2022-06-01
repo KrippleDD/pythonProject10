0fcbea85a407b75c5d65183029efb704caab9371
KrippleDD <kirill.drik.717@gmail.com>
2022-06-01 15:29:00 +0300

    10

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 a = [int(i) for i in input().split()]
-counter = 0
+suma = 0
 for i in range(1, len(a) - 1):
-    # о боги, разве так можно писать?
     if a[i - 1] < a[i] > a[i + 1]:
-        counter += 1
-print(counter)
+        suma += 1
+print(suma)
